156ab7f36e47861b0ca8a7f066503e635c6ed83f
YAHYA <yahya.abdul.s87@Kalvium.community>
2025-09-15 14:12:15 +0530

    Completed student project tracker DB connection

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
+// index.js
 const express = require('express');
-const { resolve } = require('path');
+const mongoose = require('mongoose');
+require('dotenv').config(); // Load environment variables
 
 const app = express();
-const port = 3010;
+const PORT = process.env.PORT || 3000;
 
-app.use(express.static('static'));
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error(`Error connecting to MongoDB: ${error}`);
+  });
 
-app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
-});
+// Middleware and routes can be added here
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT} `);
+  console.log(`http://localhost:${PORT}`)
 });
